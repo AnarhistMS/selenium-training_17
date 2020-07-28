5b91b782e247fdfed2c87302529343bb11535939
Aleksei Kokoulin <kokoulin21@gmail.com>
2020-07-28 22:09:30 +0300

    First commit

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,13 +1,18 @@
 package ru.stqa.training.selenium;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
@@ -28,14 +33,18 @@ public class MyFirstTest {
         driver.findElement(By.xpath("//input[@type=\"text\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@value=\"Login\"]")).click();
-        wait.until(titleIs("Dashboard | My Store"));
-        int numberOfElements = driver.findElements(By.xpath("//ul[@id=\"box-apps-menu\"]/li")).size();
-        for (int i = 1; i <= numberOfElements; i++){
-            driver.findElement(By.xpath(String.format("//ul[@id=\"box-apps-menu\"]/li[%d]", i))).click();
-            int numberOfInsideElements = driver.findElements(By.xpath("//ul[@class=\"docs\"]/li")).size();
-            for (int j = 1; j <= numberOfInsideElements; j++){
-                driver.findElement(By.xpath(String.format("//ul[@class=\"docs\"]/li[%d]", j))).click();
-            }
+        wait.until(titleIs("My Store"));
+        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+        List<WebElement> ducks = driver.findElements(By.xpath("//table[@class='dataTable']//td[3]//a[contains(@href,'edit_product')]"));
+        for (int i = 0; i < ducks.size(); i++) {
+            List<WebElement> updatedDucks = driver.findElements(By.xpath("//table[@class='dataTable']//td[3]//a[contains(@href,'edit_product')]"));
+            updatedDucks.get(i).click();
+            wait.until(titleContains("Edit Product"));
+            driver.manage().logs().get("browser").getAll().forEach(message -> {
+                Assert.assertTrue(message.equals(""));
+            });
+            driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+            wait.until(titleIs("Catalog | My Store"));
         }
     }
 
